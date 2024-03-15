c2e680dca5a4ed7893b16f21b2b6256946bf310a
iamsauravsharma <appdroiddeveloper@gmail.com>
2024-03-15 09:49:32 +0000

    Deploying to gh-pages from @ iamsauravsharma/jailer@ad72d138f6cd508e58982625db4979cbe5b6060c 🚀

--- trait.impl/core/marker/trait.Freeze.js
+++ trait.impl/core/marker/trait.Freeze.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"jailer":[["impl Freeze for <a class=\"struct\" href=\"jailer/struct.Jailer.html\" title=\"struct jailer::Jailer\">Jailer</a>",1,["jailer::Jailer"]]]
+"jailer":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"jailer/struct.Jailer.html\" title=\"struct jailer::Jailer\">Jailer</a>",1,["jailer::Jailer"]]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
