a592b1ae080e8e46966c81b4ff66a21c16e21a1d
iamsauravsharma <appdroiddeveloper@gmail.com>
2025-09-03 12:40:53 +0000

    Deploying to gh-pages from @ iamsauravsharma/jailer@9068710f8c2ce813dcbe1e86286c88bb0ec894e7 🚀

--- src-files.js
+++ src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["jailer",["",[],["lib.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[29]}
+createSrcSidebar('[["jailer",["",[],["lib.rs"]]]]');
+//{"start":19,"fragment_lengths":[29]}
---
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["jailer",[["impl UnsafeUnpin for <a class=\"struct\" href=\"jailer/struct.EnvJailer.html\" title=\"struct jailer::EnvJailer\">EnvJailer</a>",1,["jailer::EnvJailer"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"jailer/struct.Jailer.html\" title=\"struct jailer::Jailer\">Jailer</a>",1,["jailer::Jailer"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[312]}
